5614737aea05b2098abacbf7f74be147212d0604
Mohammed Ashfaque <mohammedashfaquemvkpadi@gmail.com>
2025-08-11 10:02:07 +0530

    input name fixed succesed

--- src/features/auth/Register.jsx
+++ src/features/auth/Register.jsx
@@ -3,7 +3,7 @@ import { useAuth } from "../../context/AuthContext";
 import { toast } from "react-toastify";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import Input from '../../Components/Input'
+import Input from '../../components/Input'
 
 const Register = () => {
   const { register } = useAuth();
